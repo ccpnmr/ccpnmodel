94ca78cf50bf42fd545ef62cac1d25903febad20
wb104 <wb104@cam.ac.uk>
2017-07-06 17:04:11 +0100

    Strip out blank lines in FASTA format, and also remove leading > from compound name. Remove redundant file, FastaIo.py.

--- ccpncore/lib/Io/Fasta.py
+++ ccpncore/lib/Io/Fasta.py
@@ -1,17 +1,32 @@
+"""Code for reading Fasta format files
 
-# def isFastaFormat(inputFile):
-#
-#   if inputFile.split('.')[-1] == 'fasta':
-#     return True
-#   else:
-#     with open(inputFile, 'r') as f:
-#       try:
-#         if f.readlines()[0].startswith('>'):
-#           return True
-#       except:
-#         pass
-#
-#   return False
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan"
+               "Simon P Skinner & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2017-04-07 11:41:32 +0100 (Fri, April 07, 2017) $"
+__version__ = "$Revision: 3.0.b1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: CCPN $"
+
+__date__ = "$Date: 2017-04-07 10:28:48 +0000 (Fri, April 07, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
 
 
 def parseFastaFile(inputFile):
@@ -20,12 +35,12 @@ def parseFastaFile(inputFile):
   sequences = []
   with open(inputFile, 'r') as f:
     chains = []
-    lines = [line.strip() for line in f.readlines()][:-1]
+    lines = [line.strip() for line in f.readlines() if line.strip()]
     for line in lines:
       if line and line[0] == '>':
         chains.append(lines.index(line))
   for chain in chains :
-    name = lines[chain].split()[0]
+    name = lines[chain][1:].lstrip().split()[0] # the [1:] to eliminate the '>'
     index = chains.index(chain)
     if not index == len(chains)-1:
       endIndex = chains[index+1]
